49a9cb6b11de8ccea8c4e2070c0fd7c383c81819
Fujiwara <jli083@mymail.sim.edu.sg>
2022-08-10 14:23:43 +0800

    Test Push

--- screens/ResumeScreen.js
+++ screens/ResumeScreen.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ResumeScreen() {
   return (
     <Text>
-      ResumeScreen Test Push 
+      ResumeScreen Test Push jia xuan add this
     </Text>
   )
 }
