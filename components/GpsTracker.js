ccf7af4f60fea16829ccabefab13653053f5349f
DKJKOH <kohDamien1998@gmail.com>
2022-08-18 23:59:55 +0800

    Edits to the GpsTracker Screen

--- components/GpsTracker.js
+++ components/GpsTracker.js
@@ -2,19 +2,251 @@ import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions, Image} from 'react-native';
 import * as Location from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
-import MapView from 'react-native-maps';
-import {Marker, Callout} from 'react-native-maps';
 
-
+// Additional stuff to provide mapstyling for IOS
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 var lat = 1.381962975468755;
 var long = 103.72586352182203;
 
+// Map Json Styling Taken From: https://mapstyle.withgoogle.com/
+const customStyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8ec3b9"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1a3646"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#64779e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.province",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.man_made",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#334e87"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6f9ba5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3C7680"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#304a7d"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2c6675"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#255763"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#b0d5ce"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3a4762"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#0e1626"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#4e6d70"
+      }
+    ]
+  }
+]
+
 const LOCATION_TRACKING = 'location-tracking';
 function UserLocation() {
 const [locationStarted, setLocationStarted] = React.useState(false);
-
-
 
 
 const startLocationTracking = async () => {
@@ -95,38 +327,18 @@ return (
 
           <View style = {styles.container}>
             <MapView
+              provider = {PROVIDER_GOOGLE}
               onMapLoaded={startLocation}
               style={styles.map} 
-              initialRegion = {{
-                latitude: lat,
-                longitude: long,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0922
-              }}>
-              <Marker
-                coordinate={{
-                  latitude: lat,
-                  longitude: long
-                }}
-                tracksViewChanges={true}>
-                {/* Marker Indicating User's Location */}
-                <Callout>
-                  <Text>Jane Doe's Location</Text>
-                </Callout>
-
-              </Marker>
+              showsUserLocation = {true}
+              // Map will focus on user's Location (apple maps only)
+              followsUserLocation = {true}
+              showsTraffic = {true}
+              showsPointsOfInterest = {true}
+              customMapStyle = {customStyle}
+              >
             </MapView>
-
           </View>
-            {locationStarted ?
-                <TouchableOpacity onPress={stopLocation}>
-                    <Text style={styles.btnText}>Stop Tracking</Text>
-                </TouchableOpacity>
-                :
-                <TouchableOpacity onPress={startLocation}>
-                    <Text style={styles.btnText}>Start Tracking</Text>
-                </TouchableOpacity>
-            }
         </View>
     );
 }
@@ -152,7 +364,7 @@ const styles = StyleSheet.create({
     },
     map: {
       width: Dimensions.get('window').width / 1.2,
-      height: Dimensions.get('window').height / 2.3,
+      height: Dimensions.get('window').height / 1.95,
     }
 
 });
@@ -167,10 +379,8 @@ TaskManager.defineTask(LOCATION_TRACKING, async ({ data, error }) => {
         const { locations } = data;
         lat = locations[0].coords.latitude;
         long = locations[0].coords.longitude;
-        // Print out the current user co-ordinates
-console.log(
-            `${new Date(Date.now()).toLocaleString()}: ${lat},${long}`
-        );
+        // Print out the current user co-ordinates 
+        console.log(`${new Date(Date.now()).toLocaleString()}: ${lat},${long}`);
     }
 });
 export default UserLocation;
