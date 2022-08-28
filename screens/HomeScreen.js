8004e23159e28fa525c0f1bdc0d4ed12d4e7d9e6
Nig3lTseng <nigeltseng8@gmail.com>
2022-08-28 22:03:32 +0800

    Styling breakdown for HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,10 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import React from "react";
 import Header from "../components/Header";
 import Categories from "../components/Categories";
@@ -21,55 +27,22 @@ const HomeScreen = () => {
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       {/* Header for the application */}
-      <View style={{ backgroundColor: "#2C66FF", padding: 20}}>
+      <View style={{ backgroundColor: "#2C66FF", padding: 20 }}>
         {/* Home Button */}
         <TouchableOpacity
-          style=
-          {{
-            backgroundColor: "#2C66FF",
-            paddingVertical: 4,
-            paddingHorizontal: 16,
-            borderRadius: 30,
-            // Ensures TaskCloud Logo is Centred
-            alignSelf: 'center'
-          }}
-          onPress={() => navigation.navigate("Home")}>
-         <Header style={{ background: 'blue', flex: 1}}/>
+          style={styles.head}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Header style={{ background: "blue", flex: 1 }} />
         </TouchableOpacity>
       </View>
 
-
       {/* <SearchBar /> */}
       <Categories />
-      <View
-        style={{
-          backgroundColor: "#2C66FF",
-          width: "100%",
-          padding: 15,
-          alignItems: "center",
-          borderColor: "black",
-          borderWidth: 1,
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-            fontWeight: "220",
-            fontSize: 14,
-          }}
-        >
-          Email: {auth.currentUser?.email}
-        </Text>
+      <View style={styles.signOutContainer}>
+        <Text style={styles.signOutMsg}>Email: {auth.currentUser?.email}</Text>
         <TouchableOpacity onPress={handleSignOut}>
-          <Text
-            style={{
-              color: "white",
-              fontWeight: "700",
-              fontSize: 16,
-            }}
-          >
-            Sign out
-          </Text>
+          <Text style={styles.signOutButton}>Sign out</Text>
         </TouchableOpacity>
       </View>
       <ListingItems />
@@ -78,3 +51,35 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+/*----------------------Styling-------------------*/
+const styles = StyleSheet.create({
+  head: {
+    backgroundColor: "#2C66FF",
+    paddingVertical: 4,
+    paddingHorizontal: 16,
+    borderRadius: 30,
+    alignSelf: "center",
+  },
+
+  signOutContainer: {
+    backgroundColor: "#2C66FF",
+    width: "100%",
+    padding: 15,
+    alignItems: "center",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+
+  signOutMsg: {
+    color: "white",
+    fontWeight: "220",
+    fontSize: 14,
+  },
+
+  signOutButton: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
